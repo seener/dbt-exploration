0ad685496a9c6a756e15da7c8177efc5b5c518b1
Seener <seener49@gmail.com>
2020-08-12 17:39:31 -0400

    remove the stuff that should be getting ignored

--- target/compiled/dbt_play/models/schema.yml/schema_test/not_null_customer_profile_active.sql
+++
@@ -1,8 +0,0 @@
-
-
-
-
-select count(*) as validation_errors
-from "dvd_rental"."dev"."customer_profile"
-where active is null
-
--- target/compiled/dbt_play/models/staging/customer_life_time_value.sql
+++
@@ -1,9 +0,0 @@
-with customer_life_time_value as (
-    SELECT
-        customer_id,
-        sum(amount) AS life_time_value
-    FROM dvd_rental.public.payment
-    GROUP BY customer_id
-)
-
-SELECT * FROM customer_life_time_value
