b68ec30d96948222aee34dec79bb6027c20dcb11
Seener <seener49@gmail.com>
2020-08-12 19:56:10 -0400

    add dbt_module to pivot rentals table to get counts

---
+++ models/staging/customer_by_category.sql
@@ -0,0 +1,12 @@
+with customer_by_category as (
+    SELECT 
+        customer_id,
+        {{dbt_utils.pivot(
+            'film_category',
+            dbt_utils.get_column_values(ref('rental_details'), 'film_category')
+        )}}
+    FROM {{ref('rental_details')}}
+    GROUP BY customer_id
+)
+
+SELECT * FROM customer_by_category
